027abc114e4714cf322f5839f353892e3663dfda
PetrCala <cala.p@seznam.cz>
2025-01-04 17:54:54 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// .eslintrc.js
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+		"plugin:prettier/recommended",
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: "module",
+	},
+	rules: {
+		// Customize your rules
+	},
+}
